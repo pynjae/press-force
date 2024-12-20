65181411dc8cb48635381081c8a9eb776b17b25c
박윤재 <pynjae@soltray.co.kr>
2024-12-20 16:10:46 +0900

    [추가] 1. 프로젝트 최초생성.

---
+++ src/main/java/com/together/press_force/PressForceApplication.java
@@ -0,0 +1,13 @@
+package com.together.press_force;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PressForceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PressForceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/together/press_force/PressForceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.together.press_force;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PressForceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
